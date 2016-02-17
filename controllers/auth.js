07c22d666c17a36418b8d21feab21f96acbfcf8d
katiosity <lovan.kathy@gmail.com>
2016-02-17 14:09:53 -0800

    added user signup functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,34 @@
 var express = require('express');
+var db = require('../models');
 var router = express.Router();
+
 
 router.get('/signup', function(req, res) {
   res.render('auth/signup');
 });
 
 router.post('/signup', function(req, res) {
-  res.send(req.body);
+  // res.send(req.body);
+  var name = req.body.name;
+  var email = req.body.email;
+  var password = req.body.password;
+  db.user.findOrCreate({
+  	where: {
+  		email: email
+  	},
+  	defaults: {
+  		name: name,
+  		password: password
+  	}
+  }).spread(function(user, created) {
+	  	if (created) {
+	  		res.redirect('/');
+	  	} else {
+	  		res.send('User already exists');
+	  	}
+	}).catch(function(err) {
+	 	res.send(err);
+  });
 });
 
 router.get('/login', function(req, res) {
